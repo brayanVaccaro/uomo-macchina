7d6583720f194979d84fc1cdbd3478f0e79d6b86
Brayan Vaccaro <brayan.vaccaro@studio.unibo.it>
2024-01-28 21:49:11 +0100

    feat: file main gestisce la logica in maniera corretta, file commentato

--- src/Web/wwwroot/js/main.js
+++ src/Web/wwwroot/js/main.js
@@ -1,189 +1,89 @@
-//Vue 2
-/*
-let app = new Vue({
-    el: '#pageId',
-    data: {
-        message: `Vue works!`,
-        event: [],
-    },
-    mounted: function () {
-        this.fetchEvents();
-    },
-    methods: {
-        fetchEvents: async function () {
-            console.log("chiamo fetch")
-            let prova = await fetch('/Main/Main/GetData')
-                .then(response => {
-                    if (!response.ok) {
-                        console.log(response.statusText)
-                        throw new Error('Network response was not ok ');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-
-                    this.event = data
-                    console.log("data", data)
-                    console.log("this.event",this.event)
-
-                    for (let i = 0; i < 5; i++) {
-                        const day = this.previousFirstDayOfWeek.addDays(i).format()
-                        data.forEach(x => {
-                            this.event.push(
-                                {
-                                    start: `${day} ${x.startTime}`,
-                                    end: `${day} ${x.endTime}`,
-                                    class: x.cssClass,
-                                    title: x.Title
-                                })
-                        })
-                    }
-
-                    console.log("data", data[0].start)
-                    console.log("this.event", this.event)
-
-                })
-                .catch(
-                    error => {
-                        console.error('Unable to get events')
-                        console.log(error)
-                    }
-                );
-        }
-    },
-    computed: {
-        // Get the Monday of the real time current week.
-        previousFirstDayOfWeek() {
-            return new Date(new Date().setDate(new Date().getDate() - (new Date().getDay() + 6) % 7))
-        }
-    },
-    created() {
-        // Place all the events in the real time current week.
-        for (let i = 0; i < 5; i++) {
-            const day = this.previousFirstDayOfWeek.addDays(i).format()
-            console.log("day vale =", day)
-
-            this.event.push(
-                {
-                    start: `${day} 13:00`,
-                    end: `${day} 14:00`,
-                    title: 'LUNCH',
-                    class: 'lunch',
-                    background: true,
-                    deletable: false,
-                    resizable: false,
-                    //split: 1
-                },
-                {
-                    start: `${day} 13:00`,
-                    end: `${day} 14:00`,
-                    title: 'LUNCH',
-                    class: 'lunch',
-                    background: true,
-                    deletable: false,
-                    resizable: false,
-                    //split: 2
-                }
-            )
-        }
-        console.log(this.event)
-    },
-    components: {
-        "vue-cal": vuecal,
-
-    }
-
-})
-})
-*/
-//console.log(VueCal)
+//accedo a @Model
 ////Vue 3
 const app = Vue.createApp({
     data() {
         return {
+            model: mainModel, //oggetto model locale, da poter modificare
+            mostraSettimana: true,
+            mostraGiorno: false,
             message: `Vue works!`,
-            activeView: "week",
+            activeView: "week", //la vista attiva, fra day e week
+            selectedDate: "",
+            customClass: "col", //classe custom per ridurre la larghezza quando si seleziona activeView=day
             selectedEvent: [],
             singleDay: false,
-            showDialog: false,
-            event: [],
+            showSingleDay: false,
+            event: [], //eventi presi dal backend
         };
     },
     mounted() {
-        this.fetchEvents();
+        //setto gli eventi
+        this.setEvents();
     },
     methods: {
-        async fetchEvents() {
-            console.log("chiamo fetch");
-            await fetch('/Main/Main/GetData')
-                .then(response => {
+        //visualizzo sulla destra gli eventi per il giorno selezionato
+        async setDataByDate(dataScelta) {
+            const url = `/Main/Main/GetData?dataScelta=${encodeURIComponent(dataScelta)}`;
+            try {
+                const response = await fetch(url);
                 if (!response.ok) {
-                    console.log(response.statusText);
-                    throw new Error('Network response was not ok ');
+                    throw new Error(`Network response was not ok, status: ${response.status}`);
                 }
-                return response.json();
-            })
-                .then(data => {
-                console.log("data", data);
-                //come prova inserisco manualmente l'unico evento nel db per tutti i giorni della settimana corrente
-                for (let i = 0; i < 5;) {
-                    const day = this.previousFirstDayOfWeek.addDays(i).format();
-                    data.forEach(x => {
-                        //pusho gli eventi presi dal backend
-                        this.event.push({
-                            // questi sono i parametri che il componente si aspetta per gli eventi
-                            start: `${day} ${x.startTime}`, //questo è dataInizio + orario dell'evento
-                            end: `${day} ${x.endTime}`,
-                            title: x.Title,
-                            content: x.content,
-                            class: x.cssClass,
-                            background: x.background,
-                            spli: x.split,
-                            allDay: x.allDay,
-                            deletable: x.deletable,
-                            resizable: x.resizable,
-                            //da qui in poi i valori che uso localmente, quindi non richiesti dal componente ma che mi servono per visualizzarli al click
-                            id: x.id,
-                            startDate: x.startDate,
-                            startTime: x.starTime,
-                        });
-                    });
-                    i++;
-                }
-                //console.log("data", data[0].start)
-                console.log("this.event", this.event);
-            })
-                .catch(error => {
-                console.error('Unable to get events');
-                console.log(error);
+                const data = await response.json();
+                this.model = data;
+                console.log("this.model.ferie..lenght", this.model.ferie.ferie.length);
+            }
+            catch (error) {
+                console.error('Unable to get events', error);
+                throw new Error(`Network response was not ok, error: ${error}`);
+            }
+        },
+        //metodo per settare gli eventi del backend nel calendario vue-cal
+        setEvents() {
+            this.model.eventi.eventi.forEach(x => {
+                //pusho gli eventi presi dal backend
+                this.event.push({
+                    // questi sono i parametri che il componente si aspetta per gli eventi
+                    start: `${x.startDate} ${x.startTime}`, //questo è dataInizio + orario dell'evento
+                    end: `${x.endDate} ${x.endTime}`,
+                    title: x.title,
+                    content: x.content,
+                    class: x.class,
+                    background: x.background,
+                    split: x.split,
+                    allDay: x.allDay,
+                    deletable: x.deletable,
+                    resizable: x.resizable,
+                    //da qui in poi i valori che uso localmente, quindi non richiesti dal componente ma che mi servono per visualizzarli al click
+                    id: x.id,
+                    startDate: x.startDate,
+                    startTime: x.starTime,
+                });
             });
         },
-        onEventClickOld(event, e) {
-            //Reindirizzamento alla pagina con il singolo giorno
-            //fetch(globalModelData)
-            //console.log("globalModelData", globalModelData) // ho i dati dell'evento che ho appena cliccato
-            console.log(event); // ho i dati dell'evento che ho appena cliccato
-            console.log(e); // errore
-            //window.location.href = '/Main/SingleDay';
+        //metodo per gestire cosa succede se si sceglie di visualizzare una settimana
+        selezionaSettimana(idSettimana) {
+            console.log("ho cliccato sulla settimana");
+            this.mostraGiorno = false;
+            this.mostraSettimana = true;
+            this.showSingleDay = false;
+            this.customClass = "col";
+            this.activeView = "week";
         },
-        async onEventClick(event, e) {
-            console.log("sono in andiamo event", event);
+        //metodo per gestire cosa succede se si sceglie di visualizzare un giorno, idGiorno è il giorno scelto
+        async selezionaGiorno(idGiorno) {
+            this.selectedDate = idGiorno;
+            await this.setDataByDate(this.selectedDate);
+            this.mostraSettimana = false;
+            this.mostraGiorno = true;
+            this.customClass = "col-2";
+            this.showSingleDay = true;
+            this.activeView = "day";
+        },
+        onEventClick(event, e) {
             this.selectedEvent = event;
-            this.showDialog = true;
+            console.log("this.selectedEvent", this.selectedEvent);
             e.stopPropagation();
-            // Seleziona l'elemento basato sull'ID o sulla classe
-            //const element = document.querySelector('.vuecal__flex.vuecal');
-            //if (element) {
-            //    // Rimuovi la classe 'vuecal--week-view'
-            //    element.classList.remove('vuecal--week-view');
-            //    console.log("ho rimosso",element.classList)
-            //    // Aggiungi la classe 'vuecal--day-view'
-            //    element.classList.add('vuecal--day-view');
-            //    console.log("ho aggiunto",element.classList)
-            //}
-            //this.activeView = 'day'
-            //await fetch('/Main/Main/SingleDay')
-            // Prevent navigating to narrower view (default vue-cal behavior).
         }
     },
     computed: {
@@ -207,12 +107,23 @@ const app = Vue.createApp({
                 resizable: false,
             });
         }
-        //console.log(globalModelData)
-        //console.log(this.event)
+        // Inserisco in sabato e domenica il giorno feriale
+        for (let i = 5; i <= 6; i++) {
+            const day = this.previousFirstDayOfWeek.addDays(i).format();
+            //console.log("day vale =", day)
+            this.event.push({
+                start: `${day} 13:00`,
+                end: `${day} 14:00`,
+                title: 'giorno feriale',
+                class: 'libero',
+                allDay: true,
+                background: true,
+                deletable: false,
+                resizable: false,
+            });
+        }
     },
+    components: { 'vue-cal': vuecal }
 });
-app.component('vue-cal', vuecal);
 app.mount('#pageId');
-// Esporta l'istanza di Vue per un uso esterno
-//export default app;
 //# sourceMappingURL=main.js.map
--- src/Web/wwwroot/js/main.ts
+++ src/Web/wwwroot/js/main.ts
@@ -1,211 +1,105 @@
-﻿//import { defineComponent } from "vue";
-//import * as Vue from "./bundle-vue.js";
-//import vuecal from "./bundle-vue-cal.js"
-//import * as vuecal from "./bundle-vue-cal.js"
-declare var Vue: any;
+﻿declare var Vue: any;
 declare var vuecal: any;
+declare var mainModel: any; //variabile globale che rappresenta il mainViewModel
 
-declare var globalModelData: any; //accedo a @Model
-//Vue 2
-/*
-let app = new Vue({
-    el: '#pageId',
-    data: {
-        message: `Vue works!`,
-        event: [],
-    },
-    mounted: function () {
-        this.fetchEvents();
-    },
-    methods: {
-        fetchEvents: async function () {
-            console.log("chiamo fetch")
-            let prova = await fetch('/Main/Main/GetData')
-                .then(response => {
-                    if (!response.ok) {
-                        console.log(response.statusText)
-                        throw new Error('Network response was not ok ');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-
-                    this.event = data
-                    console.log("data", data)
-                    console.log("this.event",this.event)
-
-                    for (let i = 0; i < 5; i++) {
-                        const day = this.previousFirstDayOfWeek.addDays(i).format()
-                        data.forEach(x => {
-                            this.event.push(
-                                {
-                                    start: `${day} ${x.startTime}`,
-                                    end: `${day} ${x.endTime}`,
-                                    class: x.cssClass,
-                                    title: x.Title
-                                })
-                        })
-                    }
-
-                    console.log("data", data[0].start)
-                    console.log("this.event", this.event)
-
-                })
-                .catch(
-                    error => {
-                        console.error('Unable to get events')
-                        console.log(error)
-                    }
-                );
-        }
-    },
-    computed: {
-        // Get the Monday of the real time current week.
-        previousFirstDayOfWeek() {
-            return new Date(new Date().setDate(new Date().getDate() - (new Date().getDay() + 6) % 7))
-        }
-    },
-    created() {
-        // Place all the events in the real time current week.
-        for (let i = 0; i < 5; i++) {
-            const day = this.previousFirstDayOfWeek.addDays(i).format()
-            console.log("day vale =", day)
-
-            this.event.push(
-                {
-                    start: `${day} 13:00`,
-                    end: `${day} 14:00`,
-                    title: 'LUNCH',
-                    class: 'lunch',
-                    background: true,
-                    deletable: false,
-                    resizable: false,
-                    //split: 1
-                },
-                {
-                    start: `${day} 13:00`,
-                    end: `${day} 14:00`,
-                    title: 'LUNCH',
-                    class: 'lunch',
-                    background: true,
-                    deletable: false,
-                    resizable: false,
-                    //split: 2
-                }
-            )
-        }
-        console.log(this.event)
-    },
-    components: {
-        "vue-cal": vuecal,
-
-    }
-
-})
-})
-*/
-//console.log(VueCal)
+//accedo a @Model
 ////Vue 3
 const app = Vue.createApp({
 
     data() {
         return {
+            model: mainModel, //oggetto model locale, da poter modificare
+            mostraSettimana: true,
+            mostraGiorno: false,
             message: `Vue works!`,
-            activeView: "week",
+            activeView: "week", //la vista attiva, fra day e week
+            selectedDate: "",
+            customClass: "col", //classe custom per ridurre la larghezza quando si seleziona activeView=day
             selectedEvent: [],
             singleDay: false,
-            showDialog: false,
-            event: [],
+            showSingleDay: false,
+            event: [], //eventi presi dal backend
         };
     },
     mounted() {
-        this.fetchEvents();
+        //setto gli eventi
+        this.setEvents()
     },
     methods: {
-        async fetchEvents() {
-            console.log("chiamo fetch")
-            await fetch('/Main/Main/GetData')
-                .then(response => {
-                    if (!response.ok) {
-                        console.log(response.statusText)
-                        throw new Error('Network response was not ok ');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log("data", data)
-                    //come prova inserisco manualmente l'unico evento nel db per tutti i giorni della settimana corrente
-                    for (let i = 0; i < 5;) {
-                        const day = this.previousFirstDayOfWeek.addDays(i).format()
-                        data.forEach(x => {
-                            //pusho gli eventi presi dal backend
-                            this.event.push(
-                                {
-                                    // questi sono i parametri che il componente si aspetta per gli eventi
-                                    start: `${day} ${x.startTime}`, //questo è dataInizio + orario dell'evento
-                                    end: `${day} ${x.endTime}`,
-                                    title: x.Title,
-                                    content: x.content,
-                                    class: x.cssClass,
-                                    background: x.background,
-                                    spli: x.split,
-                                    allDay: x.allDay,
-                                    deletable: x.deletable,
-                                    resizable: x.resizable,
-                                    //da qui in poi i valori che uso localmente, quindi non richiesti dal componente ma che mi servono per visualizzarli al click
-                                    id: x.id,
-                                    startDate: x.startDate,
-                                    startTime: x.starTime,
+        //visualizzo sulla destra gli eventi per il giorno selezionato
+        async setDataByDate(dataScelta) {
+            const url = `/Main/Main/GetData?dataScelta=${encodeURIComponent(dataScelta)}`;
 
-                                })
-                        })
-                        i++;
-                    }
+            try {
+                const response = await fetch(url);
+                if (!response.ok) {
+                    throw new Error(`Network response was not ok, status: ${response.status}`);
+                }
+                const data = await response.json();
+                
+                this.model = data
+                console.log("this.model.ferie..lenght", this.model.ferie.ferie.length)
 
-                    //console.log("data", data[0].start)
-                    console.log("this.event", this.event)
-                })
-                .catch(
-                    error => {
-                        console.error('Unable to get events')
-                        console.log(error)
-                    }
-                );
+            }
+            catch (error) {
+                console.error('Unable to get events', error);
+                throw new Error(`Network response was not ok, error: ${error}`);
+            }
         },
-        onEventClickOld(event,e) {
-            //Reindirizzamento alla pagina con il singolo giorno
-            //fetch(globalModelData)
-            
-            //console.log("globalModelData", globalModelData) // ho i dati dell'evento che ho appena cliccato
-            console.log(event) // ho i dati dell'evento che ho appena cliccato
-            console.log(e) // errore
 
-            
-            //window.location.href = '/Main/SingleDay';
+        //metodo per settare gli eventi del backend nel calendario vue-cal
+        setEvents() {
+            this.model.eventi.eventi.forEach(x => {
+                //pusho gli eventi presi dal backend
+                this.event.push(
+                    {
+                        // questi sono i parametri che il componente si aspetta per gli eventi
+                        start: `${x.startDate} ${x.startTime}`, //questo è dataInizio + orario dell'evento
+                        end: `${x.endDate} ${x.endTime}`,
+                        title: x.title,
+                        content: x.content,
+                        class: x.class,
+                        background: x.background,
+                        split: x.split,
+                        allDay: x.allDay,
+                        deletable: x.deletable,
+                        resizable: x.resizable,
+                        //da qui in poi i valori che uso localmente, quindi non richiesti dal componente ma che mi servono per visualizzarli al click
+                        id: x.id,
+                        startDate: x.startDate,
+                        startTime: x.starTime,
+
+                    })
+            })
         },
-        async onEventClick(event, e) {
+        //metodo per gestire cosa succede se si sceglie di visualizzare una settimana
+        selezionaSettimana(idSettimana) {
+            console.log("ho cliccato sulla settimana")
+
+            this.mostraGiorno = false;
+            this.mostraSettimana = true;
+            this.showSingleDay = false
+
+            this.customClass = "col"
+            this.activeView = "week";
+
+        },
+        //metodo per gestire cosa succede se si sceglie di visualizzare un giorno, idGiorno è il giorno scelto
+        async selezionaGiorno(idGiorno) {
+            this.selectedDate = idGiorno
+            await this.setDataByDate(this.selectedDate)
             
-            console.log("sono in andiamo event", event)
+            this.mostraSettimana = false;
+            this.mostraGiorno = true;
+            this.customClass = "col-2"
+            this.showSingleDay = true
+            this.activeView = "day";
+        },
+
+        onEventClick(event, e) {
             this.selectedEvent = event
-            this.showDialog = true
+            console.log("this.selectedEvent", this.selectedEvent)
             e.stopPropagation()
-            // Seleziona l'elemento basato sull'ID o sulla classe
-            //const element = document.querySelector('.vuecal__flex.vuecal');
-
-            //if (element) {
-            //    // Rimuovi la classe 'vuecal--week-view'
-            //    element.classList.remove('vuecal--week-view');
-            //    console.log("ho rimosso",element.classList)
-            //    // Aggiungi la classe 'vuecal--day-view'
-            //    element.classList.add('vuecal--day-view');
-
-            //    console.log("ho aggiunto",element.classList)
-            //}
-            //this.activeView = 'day'
-            //await fetch('/Main/Main/SingleDay')
-
-            // Prevent navigating to narrower view (default vue-cal behavior).
-
         }
     },
     computed: {
@@ -234,14 +128,25 @@ const app = Vue.createApp({
                 }
             )
         }
-        //console.log(globalModelData)
-        //console.log(this.event)
+        // Inserisco in sabato e domenica il giorno feriale
+        for (let i = 5; i <= 6; i++) {
+
+            const day = this.previousFirstDayOfWeek.addDays(i).format();
+            //console.log("day vale =", day)
+
+            this.event.push({
+                start: `${day} 13:00`,
+                end: `${day} 14:00`,
+                title: 'giorno feriale',
+                class: 'libero',
+                allDay: true,
+                background: true,
+                deletable: false,
+                resizable: false,
+            });
+        }
     },
+    components: { 'vue-cal': vuecal }
 });
 
-app.component('vue-cal', vuecal);
 app.mount('#pageId');
-
-// Esporta l'istanza di Vue per un uso esterno
-//export default app;
-
