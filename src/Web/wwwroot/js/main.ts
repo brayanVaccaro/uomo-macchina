afc76d3df41937bc365fefcb732a782d3abd27f0
Brayan Vaccaro <brayan.vaccaro@studio.unibo.it>
2024-02-11 17:25:59 +0100

    feat: resa dinamica la sidebar

--- src/Web/wwwroot/js/main.ts
+++ src/Web/wwwroot/js/main.ts
@@ -2,6 +2,8 @@ declare var Vue: any;
 declare var vuecal: any;
 declare var mainModel: any; //variabile globale che rappresenta il mainViewModel
 declare var ferieEditURL: any;
+declare var annoDati: any
+var currentMonthIndex = new Date().getMonth(); // 0-11 for Jan-Dec
 
 //accedo a @Model
 ////Vue 3
@@ -91,7 +93,88 @@ const app = Vue.createApp({
         //visualizzo sulla destra gli eventi per il giorno selezionato
 
 
-        /* Funzione del filtro setDataByDate */
+        updateMonthName(monthIndex) { //aggiorna il mese
+            var monthName = annoDati.Mesi[monthIndex].NomeMese;
+            document.getElementById('monthDisplay').innerText = monthName;
+        },
+
+        updateWeeksAndDays(monthIndex) {//aggiorna le settimane e i giorni
+            var settimaneContainer = document.getElementById('settimaneContainer');
+            settimaneContainer.innerHTML = ''; // Pulisce le settimane esistenti
+
+            annoDati.Mesi[monthIndex].Settimane.forEach(function (settimana) {
+                var weekElement = document.createElement('li'); // Creo un nuovo elemento di lista per la settimana
+                weekElement.className = 'nav-item dropdown col'; // Imposto le classi necessarie
+
+                var weekLink = document.createElement('a');
+                weekLink.className = 'nav-link weekDisplay';
+                weekLink.setAttribute('role', 'button');
+                weekLink.setAttribute('data-bs-toggle', 'dropdown');
+                weekLink.setAttribute('aria-expanded', 'false');
+                weekLink.setAttribute('data-giorno', settimana.Giorni[0].split('T')[0]); // Salvo il primo giorno della settimana
+                weekLink.innerText = settimana.Nome; // Imposto il nome della settimana
+
+                weekElement.appendChild(weekLink);
+
+                var daysList = document.createElement('ul');
+                daysList.className = 'dropdown-menu row'; // Imposta le classi necessarie
+
+                settimana.Giorni.forEach(function (giorno) {
+                    var dayItem = document.createElement('li');
+                    dayItem.className = 'dropdown-item dayDisplay'; // Imposta le classi necessarie
+                    dayItem.setAttribute('data-giorno', giorno.split('T')[0])
+
+                    var dayLink = document.createElement('a');
+                    var giornoDate = new Date(giorno);
+                    dayLink.innerText = giornoDate.toLocaleDateString('it-IT', { weekday: 'long', day: 'numeric' }); // Formatta e imposta il giorno
+                    dayItem.appendChild(dayLink);
+
+                    daysList.appendChild(dayItem); // Aggiunge il giorno alla lista dei giorni
+                });
+
+                weekElement.appendChild(daysList); // Aggiunge la lista dei giorni all'elemento della settimana
+                settimaneContainer.appendChild(weekElement); // Aggiunge l'elemento della settimana al container delle settimane
+            });
+            this.aggiungiEventListeners()
+        },
+
+        aggiungiEventListeners() {// aggiunge event listener alle settimane e ai giorni
+            const settimaneEl = document.querySelectorAll('.weekDisplay'); // Seleziona tutti i link delle settimane
+            settimaneEl.forEach(el => {
+                var settimanaEl = el as HTMLElement
+                settimanaEl.addEventListener('click', () => {
+                    const primoGiorno = el.getAttribute("data-giorno")
+                    this.selezionaSettimana(primoGiorno)
+                });
+            });
+
+            const giorniEl = document.querySelectorAll('.dayDisplay'); // Seleziona tutti gli elementi dei giorni
+            giorniEl.forEach(el => {
+                var giornoEl = el as HTMLElement
+                giornoEl.addEventListener('click', () => {
+                    const giorno = el.getAttribute("data-giorno")
+                    this.selezionaGiorno(giorno)
+                });
+            });
+        },
+
+        prevMonth() {//click sulla freccia sinistra
+            console.log("ho cliccato prevMonth")
+            if (currentMonthIndex > 0) {
+                currentMonthIndex--;
+                this.updateMonthName(currentMonthIndex);
+                this.updateWeeksAndDays(currentMonthIndex);
+            }
+        },
+
+        nextMonth() {//click sulla freccia destra
+            console.log("ho cliccato nextMonth")
+            if (currentMonthIndex < annoDati.Mesi.length - 1) {
+                currentMonthIndex++;
+                this.updateMonthName(currentMonthIndex);
+                this.updateWeeksAndDays(currentMonthIndex);
+            }
+        },
 
         async setDataByDate(dataScelta) {
             const url = `/Main/Main/GetData?dataScelta=${encodeURIComponent(dataScelta)}`;
