305e4f96b3e9d58952dc95b7c8ae38be0ee91423
Brayan Vaccaro <brayan.vaccaro@studio.unibo.it>
2024-02-11 17:15:57 +0100

    feat: recupero eventi filtrati per data

--- src/Web/Areas/Main/Controllers/MainController.cs
+++ src/Web/Areas/Main/Controllers/MainController.cs
@@ -149,6 +149,7 @@ namespace UomoMacchina.Areas.Main
             if (DateTime.TryParseExact(dataScelta, "yyyy-MM-dd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime dateFilter))
             {
                 RendicontazioniDTO rendicontazioni = await _sharedService.GetAllRendicontazioniByDate(dateFilter);
+                EventiDTO eventi = await _sharedService.GetAllEvents();
                 FerieDTO ferie = await _sharedService.GetAllFerieByDate(dateFilter);
                 PermessiDTO permessi = await _sharedService.GetAllPermessiByDate(dateFilter);
                 NottiFuoriDTO nottiFuori = await _sharedService.GetAllNottiFuoriByDate(dateFilter);
@@ -158,6 +159,7 @@ namespace UomoMacchina.Areas.Main
                 var model = new MainViewModel();
 
                 model.Rendicontazioni.SetRendicontazioni(rendicontazioni);
+                model.Eventi.SetEventi(eventi);
                 model.Ferie.SetFerie(ferie);
                 model.Permessi.SetPermessi(permessi);
                 model.NottiFuori.SetNottiFuori(nottiFuori);
@@ -223,55 +225,6 @@ namespace UomoMacchina.Areas.Main
 
             return annoModel;
         }
-
-
-        //private static List<SettimanaViewModel> CalcolaSettimaneDelMese(DateTime data)
-        //{
-        //    var settimaneModel = new SettimanaViewModel();
-        //    settimaneModel.Settimane = new List<SettimanaViewModel.Settimana>();
-
-        //    var cultura = CultureInfo.CurrentCulture;
-        //    var primoDelMese = new DateTime(data.Year, data.Month, 1);
-        //    var ultimoDelMese = primoDelMese.AddMonths(1).AddDays(-1);
-
-        //    // Inizializza numeroSettimanaCorrente con il numero della settimana del primo giorno del mese
-        //    var numeroSettimanaCorrente = cultura.Calendar.GetWeekOfYear(primoDelMese, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
-
-        //    // Inizializza una nuova settimana all'inizio
-        //    var settimanaCorrente = new SettimanaViewModel.Settimana
-        //    {
-        //        Nome = "Settimana " + numeroSettimanaCorrente.ToString(),
-        //        Giorni = new List<DateTime>()
-        //    };
-        //    settimaneModel.Settimane.Add(settimanaCorrente);
-
-        //    for (DateTime giorno = primoDelMese; giorno <= ultimoDelMese; giorno = giorno.AddDays(1))
-        //    {
-        //        var numeroSettimana = cultura.Calendar.GetWeekOfYear(giorno, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
-
-        //        if (numeroSettimana != numeroSettimanaCorrente)
-        //        {
-        //            numeroSettimanaCorrente = numeroSettimana;
-        //            settimanaCorrente = new SettimanaViewModel.Settimana
-        //            {
-        //                Nome = "Settimana " + numeroSettimanaCorrente.ToString(),
-        //                Giorni = new List<DateTime>()
-        //            };
-        //            settimaneModel.Settimane.Add(settimanaCorrente);
-        //        }
-
-        //        var giornoString = giorno;
-        //        settimanaCorrente.Giorni.Add(giornoString);
-        //    }
-
-        //    return settimaneModel;
-        //}
-
-
-        /**
-        devi cambiare il metodo in CalcolaMesiDiAnno(Datetime data)
-        */
-
 
     }
 }
